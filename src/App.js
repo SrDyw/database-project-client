a9b220a149e496957d590b141057acb8febcda39
SrDyw <mdayniel@gmail.com>
2023-05-18 23:30:58 -0400

    HTML UPDATE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import {AppContextProvider, AppContext} from './components/AppContext'
+import Testing from './components/Testing/Testing'
 
 export default function App() {
     // const [data, setData] = useState([{}]);
@@ -14,8 +15,8 @@ export default function App() {
 
     // console.log(x);
     return (
-        <div>
-            Proyecto BD
-        </div>
+        <>
+            <Testing/>
+        </>
     );
 }
