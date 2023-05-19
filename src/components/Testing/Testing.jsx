284c400fdd352b269418b415250bf99935b26cfa
Gian MAD <agianmichel@gmail.com>
2023-05-18 22:20:43 -0400

    Test this right now!

---
+++ src/components/Testing/Testing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Testing.css'
+
+const Testing = () => {
+  return (
+    <div className='MainFrame'>
+      <div className='first-section'>
+
+      </div>
+      <div className="bottom-section">
+        <div className="logo-profile">
+            Should be a pic
+        </div>
+        <div className="name-section">
+            Some text example
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Testing
