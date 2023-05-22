c4fdf8be2a609521f5c99b023871c70cb42d2b7d
Gian MAD <agianmichel@gmail.com>
2023-05-21 20:25:58 -0400

    Solo importe los nuevos componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
-import {AppContextProvider, AppContext} from './components/AppContext'
+import { AppContextProvider, AppContext } from "./components/AppContext";
 import Testing from "./components/Testing/Testing";
-import DesignerForm from './components/form/DesgForm'
-import EditorForm from './components/form/EditForm'
-import LevelDesignerForm from './components/form/LvlDesForm'
-import ProgrammerForm from './components/form/ProgForm'
+import DesignerForm from "./components/form/DesgForm";
+import EditorForm from "./components/form/EditForm";
+import LevelDesignerForm from "./components/form/LvlDesForm";
+import ProgrammerForm from "./components/form/ProgForm";
+import GameForm from "./components/form/GameForm";
+import IndustryForm from "./components/form/IndustryForm";
 export default function App() {
     // const [data, setData] = useState([{}]);
     // const x = useContext(AppContext);
@@ -17,7 +19,5 @@ export default function App() {
     // }, []);
 
     // console.log(x);
-    return (
-        <EditorForm></EditorForm>
-    );
+    return <EditorForm></EditorForm>;
 }
