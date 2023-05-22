57de76e2a51f3b90cf5673e8211b8af37c1caa85
Gian MAD <agianmichel@gmail.com>
2023-05-22 10:17:35 -0400

    Los nuevos cambios

---
+++ Changes/IndustryForm.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Form.css'
+
+const IndustryForm = () => {
+    return (
+        <form action="">
+            <h1 className="title">Industria</h1>
+            {/* Input section  */}
+            <div>
+                <label htmlFor="nameInput">Nombre:</label>
+                <input type="text" id="nameInput" />
+            </div>
+
+            <div className="fea-div">
+                <label htmlFor="features-input">Features</label>
+                <input type="number" name="features-input" id="features-input" min={1} max={5} step={1}/>
+            </div>
+            {/* END Input section  */}
+
+            <button onSubmit={""}>Ingresar</button>
+        </form>
+    );
+};
+
+export default IndustryForm;
+
