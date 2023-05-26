af46fcae76abaabd517d97b5e265781755537723
SrDyw <mdayniel@gmail.com>
2023-05-26 15:59:20 -0400

    v1.1

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='loading_screen'>
+        <div className='loading_symb'></div>
+        <h1>Cargando</h1>
+    </div>
+  )
+}
